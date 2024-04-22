4f359622e083e27a6cf926f8b0f33577543b9020
Maxime Fabas <maxime.fabas@pm.me>
2024-04-22 17:15:34 +0200

    idem

--- src/utils/agnostic/random/index.ts
+++ src/utils/agnostic/random/index.ts
@@ -1,5 +1,5 @@
 export function random (bound1?: number): number | undefined
-export function random (bound1: number, bound2: number | undefined): number | undefined
+export function random (bound1?: number, bound2?: number | undefined): number | undefined
 export function random (bound1: number = 1, bound2?: number | undefined): number | undefined {
   const min = bound2 === undefined ? 0 : bound1
   const max = bound2 === undefined ? bound1 : bound2

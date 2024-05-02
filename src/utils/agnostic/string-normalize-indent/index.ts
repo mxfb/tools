4d867b452a207c56b526723072c458c99100db6f
Maxime Fabas <fabas@lemonde.fr>
2024-05-02 13:23:27 +0200

    agnostic/string-normalize-indent - add pipe on line start management

--- src/utils/agnostic/string-normalize-indent/index.ts
+++ src/utils/agnostic/string-normalize-indent/index.ts
@@ -1,6 +1,13 @@
 export default function stringNormalizeIndent (input: string, indentLevel: number = 0) {
   const indent = (' ').repeat(indentLevel)
   const lines = input.split('\n')
-  const normalizedLines = lines.map(line => line.replace(/^\s*/igm, indent))
+  const noIndentLines = lines.map(line => line.replace(/^\s*/igm, ''))
+  const pipeReplacedLines = noIndentLines.map(line => {
+    const nbPipeCharsOnLineStart = line.match(/^\|+/igm)?.length ?? 0
+    const noPipeLine = line.slice(nbPipeCharsOnLineStart)
+    const replacedPipes = (' ').repeat(nbPipeCharsOnLineStart)
+    return replacedPipes + noPipeLine
+  })
+  const normalizedLines = pipeReplacedLines.map(line => indent + line)
   return normalizedLines.join('\n')
 }

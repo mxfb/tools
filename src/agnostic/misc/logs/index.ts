4380ef349de032fc2ef888d834c922e39053b1a6
Maxime Fabas <fabas@lemonde.fr>
2024-09-17 17:54:27 +0200

    src/agnostic/misc/logs - add a 'success' log output style

--- src/agnostic/misc/logs/index.ts
+++ src/agnostic/misc/logs/index.ts
@@ -23,6 +23,7 @@ export namespace Logs {
     important: (text: string) => chalk.bold(text),
     title: (text: string) => `# ${chalk.bold.underline(`${text}\n`)}`,
     info: (text: string) => chalk.blueBright(text),
+    success: (text: string) => chalk.greenBright(text),
     error: (text: string) => chalk.red(text),
     warning: (text: string) => chalk.yellow(text)
   }

7667877c06e06c929feee8eff5ffcad4496572d9
Maxime Fabas <fabas@lemonde.fr>
2024-09-19 16:00:18 +0200

    src/agnostic/misc/outcome & src/agnostic/errors - better logic separation between those two

--- src/agnostic/errors/register/index.ts
+++ src/agnostic/errors/register/index.ts
@@ -1,5 +1,3 @@
-import { Outcome } from '~/agnostic/misc/outcome'
-
 export namespace Register {
   export type RegisterEntry = {
     message: string
@@ -11,7 +9,11 @@ export namespace Register {
   export type DetailsMaker<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Source[Code]['detailsMaker']
   export type DetailsMakerParams<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Parameters<DetailsMaker<Source, Code>>
   export type Details<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = ReturnType<DetailsMaker<Source, Code>>  
-  export type FailureOutcome<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Outcome.Failure<Code, Message<Source, Code>, Details<Source, Code>>
+  export type ErrorData<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = {
+    code: Code
+    message: Message<Source, Code>,
+    details: Details<Source, Code>
+  }
   
   export function from<Source extends { [k: string]: RegisterEntry }> (source: Source) {
     function getMessage<Code extends RegisterKeys<Source>> (code: Code): Message<Source, Code> {
@@ -30,34 +32,36 @@ export namespace Register {
       return details
     }
 
-    function getFailureOutcome<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): FailureOutcome<Source, Code> {
+    function getErrorData<Code extends RegisterKeys<Source>> (
+      code: Code,
+      ...params: DetailsMakerParams<Source, Code>
+    ): ErrorData<Source, Code> {
       const message = getMessage(code)
       const details = getDetails(code, ...params)
-      return Outcome.makeFailure(code, message, details)
+      return { code, message, details }
     }
 
-    class CustomError<Code extends RegisterKeys<Source>> extends Error {
+    class RegisteredError<Code extends RegisterKeys<Source>> extends Error {
       code: Code
       details: Details<Source, Code>
       constructor (code: Code, ...params: DetailsMakerParams<Source, Code>) {
-        super(getMessage(code))
-        const details = getDetails(code, ...params)
-        this.code = code
-        this.details = details
+        const data = getErrorData(code, ...params)
+        super(data.message)
+        this.code = data.code
+        this.details = data.details
       }
     }
 
-    function getError<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): CustomError<Code> {
-      return new CustomError(code, ...params)
+    function getError<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): RegisteredError<Code> {
+      return new RegisteredError(code, ...params)
     }
 
     return {
       getMessage,
       getDetailsMaker,
       getDetails,
+      getErrorData,
       getError,
-      getFailureOutcome,
-      CustomError,
       source
     }
   }
--- src/agnostic/misc/outcome/index.ts
+++ src/agnostic/misc/outcome/index.ts
@@ -1,64 +1,14 @@
 export namespace Outcome {
-  export type Success<Payload extends any = any> = {
-    success: true,
-    payload: Payload
-  }
-
-  export type Failure<
-    Code extends any = any,
-    Msg extends string = string,
-    Details extends any = any
-  > = {
-    success: false,
-    error: {
-      code: Code,
-      message: Msg,
-      details?: Details
-    }
-  }
-
-  export type Either<
-    Payload extends any = any,
-    Code extends any = any,
-    Msg extends string = string,
-    Details extends any = any
-  > = Success<Payload> | Failure<Code, Msg, Details>
-
-  export function makeSuccess<Payload extends any> (payload: Payload): Success<Payload> {
-    return {
-      success: true,
-      payload
-    }
-  }
-
-  export function makeFailure<
-    Code extends any = any,
-    Msg extends string = string,
-    Details extends any | undefined = undefined
-  > (code: Code, message: Msg, details?: Details): Failure<Code, Msg, Details> {
-    return {
-      success: false,
-      error: {
-        code,
-        message,
-        details
-      }
-    }
-  }
-
+  export type Success<Payload extends any = any> = { success: true, payload: Payload }
+  export type Failure<Error extends any = any> = { success: false, error: Error }
+  export type Either<Payload extends any = any, Error extends any = any> = Success<Payload> | Failure<Error>
+  export function makeSuccess<Payload extends any> (payload: Payload): Success<Payload> { return { success: true, payload } }
+  export function makeFailure<Error extends any> (error: Error): Failure<Error> { return { success: false, error } }
   export function make<Payload extends any> (success: true, payload: Payload): Success<Payload>
-  export function make<
-    Code extends any = any,
-    Msg extends string = string,
-    Details extends any | undefined = undefined
-  > (success: false, code: Code, message: Msg, details?: Details): Failure<Code, Msg, Details>
-  export function make<
-    Payload extends any,
-    Code extends any = any,
-    Msg extends string = string,
-    Details extends any | undefined = undefined
-  > (success: boolean, payloadOrCode: Payload | Code, message?: Msg, details?: Details) {
-    if (success) return makeSuccess(payloadOrCode as Payload)
-    return makeFailure(payloadOrCode as Code, message as Msg, details)
+  export function make<Error extends any> (success: false, error: Error): Failure<Error>
+  export function make<Payload extends any, Error extends any> (success: boolean, payloadOrError: Payload | Error) {
+    return success
+      ? makeSuccess(payloadOrError as Payload)
+      : makeFailure(payloadOrError as Error)
   }
 }

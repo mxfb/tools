fd59ac6fc99c72b99c55652d09cc9a99d62d5ed4
Maxime Fabas <fabas@lemonde.fr>
2024-09-19 12:48:00 +0200

    src/agnostic/errors - tinkering with types

--- src/agnostic/errors/register/index.ts
+++ src/agnostic/errors/register/index.ts
@@ -5,40 +5,40 @@ export namespace Register {
     message: string
     detailsMaker: (...params: any[]) => any
   }
+
+  export type RegisterKeys<Source extends { [k: string]: RegisterEntry }> = keyof Source
+  export type Message<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Source[Code]['message']
+  export type DetailsMaker<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Source[Code]['detailsMaker']
+  export type DetailsMakerParams<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = Parameters<DetailsMaker<Source, Code>>
+  export type Details<Source extends { [k: string]: RegisterEntry }, Code extends RegisterKeys<Source>> = ReturnType<DetailsMaker<Source, Code>>  
   
   export function from<Source extends { [k: string]: RegisterEntry }> (source: Source) {
-    type RegisterKeys = keyof Source
-    type Message<Code extends RegisterKeys> = Source[Code]['message']
-    type DetailsMaker<Code extends RegisterKeys> = Source[Code]['detailsMaker']
-    type DetailsMakerParams<Code extends RegisterKeys> = Parameters<DetailsMaker<Code>>
-    type Details<Code extends RegisterKeys> = ReturnType<DetailsMaker<Code>>
-
-    function getMessage<Code extends RegisterKeys> (code: Code): Message<Code> {
+    function getMessage<Code extends RegisterKeys<Source>> (code: Code): Message<Source, Code> {
       const message = source[code]!.message
       return message
     }
 
-    function getDetailsMaker<Code extends RegisterKeys> (code: Code): DetailsMaker<Code> {
-      const maker = source[code]!.detailsMaker as DetailsMaker<Code>
+    function getDetailsMaker<Code extends RegisterKeys<Source>> (code: Code): DetailsMaker<Source, Code> {
+      const maker = source[code]!.detailsMaker as DetailsMaker<Source, Code>
       return maker
     }
 
-    function getDetails<Code extends RegisterKeys> (code: Code, ...params: DetailsMakerParams<Code>): Details<Code> {
+    function getDetails<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): Details<Source, Code> {
       const maker = getDetailsMaker(code) as undefined | ((...p: any[]) => any)
       const details = maker?.(...params)
       return details
     }
 
-    function getFailureOutcome<Code extends RegisterKeys> (code: Code, ...params: DetailsMakerParams<Code>): Outcome.Failure<Code, Message<Code>, Details<Code>> {
+    function getFailureOutcome<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): Outcome.Failure<Code, Message<Source, Code>, Details<Source, Code>> {
       const message = getMessage(code)
       const details = getDetails(code, ...params)
       return Outcome.makeFailure(code, message, details)
     }
 
-    class CustomError<Code extends RegisterKeys> extends Error {
+    class CustomError<Code extends RegisterKeys<Source>> extends Error {
       code: Code
-      details: Details<Code>
-      constructor (code: Code, ...params: DetailsMakerParams<Code>) {
+      details: Details<Source, Code>
+      constructor (code: Code, ...params: DetailsMakerParams<Source, Code>) {
         super(getMessage(code))
         const details = getDetails(code, ...params)
         this.code = code
@@ -46,7 +46,7 @@ export namespace Register {
       }
     }
 
-    function getError<Code extends RegisterKeys> (code: Code, ...params: DetailsMakerParams<Code>): CustomError<Code> {
+    function getError<Code extends RegisterKeys<Source>> (code: Code, ...params: DetailsMakerParams<Source, Code>): CustomError<Code> {
       return new CustomError(code, ...params)
     }
 

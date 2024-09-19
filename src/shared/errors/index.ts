2ff960bdd83b8ce3b674f1dbc3e3fa83530eb444
Maxime Fabas <fabas@lemonde.fr>
2024-09-19 12:38:34 +0200

    src/shared/errors - quick fix (missing detailsMaker)

--- src/shared/errors/index.ts
+++ src/shared/errors/index.ts
@@ -9,6 +9,7 @@ export enum Codes {
 export const register = Errors.Register.from({
   [Codes.NO_DOCUMENT]:                {
     message: `Runtime '${Crossenv.detectRuntime()}' does not provide a Document object, cannot complete.`,
+    detailsMaker: () => undefined
   },
   [Codes.NO_DOCUMENT_PLEASE_PROVIDE]: {
     message: `Please provide a Document object since none are found on runtime '${Crossenv.detectRuntime()}'`,

0b2fda99438ffbd88e2d88c7e78b997d4ead5c51
Maxime Fabas <fabas@lemonde.fr>
2024-10-18 18:10:51 +0200

    ~/agnostic/random - separate exports into their own files

--- src/agnostic/misc/logs/index.ts
+++ src/agnostic/misc/logs/index.ts
@@ -2,7 +2,6 @@ import { Logger as LoggerClass } from './logger'
 import { makeTextBlock as makeTextBlockFunc } from './make-text-block'
 import { styles as stylesObj } from './styles'
 
-
 export namespace Logs {
   export const makeTextBlock = makeTextBlockFunc
   export const styles = stylesObj

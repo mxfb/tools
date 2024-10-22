7f80836b2617abc924111fabb572098eb13abf3f
Maxime Fabas <fabas@lemonde.fr>
2024-10-22 11:19:31 +0200

    ~/agnostic/numbers/interpolate - add the opposite method, badly named extrerpolate

--- src/agnostic/numbers/interpolate/index.ts
+++ src/agnostic/numbers/interpolate/index.ts
@@ -1,3 +1,9 @@
 export function interpolate (ratio: number, bound1: number, bound2: number): number {
   return bound1 + ratio * (bound2 - bound1)
 }
+
+// [WIP] that name... : S
+export function exterpolate (value: number, bound1: number, bound2?: number) {
+  if (bound2 === undefined) return value / bound1
+  return (value - bound1) / (bound2 - bound1)
+}

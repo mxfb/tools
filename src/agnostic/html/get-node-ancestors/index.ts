fb9aad1c6a20f0b9cdf8b5dba729a9cab76bd2b8
Maxime Fabas <fabas@lemonde.fr>
2024-10-22 11:18:00 +0200

    ~/agnostic/html/get-node-ancestors - add traverseShadowRoots option

--- src/agnostic/html/get-node-ancestors/index.ts
+++ src/agnostic/html/get-node-ancestors/index.ts
@@ -1,10 +1,14 @@
-export function getNodeAncestors (node: Node): Element[] {
-  const returned: Element[] = []
-  let currentElement: Element | null = node.parentElement
-  while (currentElement !== null) {
-    returned.push(currentElement)
-    const newCurrentElement = currentElement.parentElement
-    currentElement = newCurrentElement
+export function getNodeAncestors (node: Node, traverseShadowRoots?: boolean) {
+  const returned: Node[] = []
+  let currentNode: Node | null = node
+  while (currentNode !== null) {
+    returned.push(currentNode)
+    const parentNode = currentNode.parentNode as ParentNode | null
+    const parentElement = currentNode.parentElement as HTMLElement | null
+    const nextNode = parentNode instanceof ShadowRoot
+      ? (traverseShadowRoots ? parentNode.host : parentElement)
+      : parentElement
+    currentNode = nextNode
   }
   return returned
 }

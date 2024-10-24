cbd1bc27bf64c4fc06ca2e15fd44e5f2193f6cfa
Maxime Fabas <fabas@lemonde.fr>
2024-10-24 17:50:25 +0200

    ~/agnostic/html/replace-in-element - can now replace with NodeList too

--- src/agnostic/html/replace-in-element/index.ts
+++ src/agnostic/html/replace-in-element/index.ts
@@ -1,12 +1,17 @@
 import { getNodeAncestors } from '../get-node-ancestors'
 
-export function replaceInElement (inputElement: Element, replaceMap: Map<Node, Node>): Element {
+export function replaceInElement (inputElement: Element, replaceMap: Map<Node, Node | NodeListOf<Node>>): Element {
   const actualReplaceMap = new Map(Array.from(replaceMap).filter(([toReplace]) => {
     const toReplaceAncestors = getNodeAncestors(toReplace)
     return toReplaceAncestors.includes(inputElement)
   }))
   actualReplaceMap.forEach((replacer, toReplace) => {
-    toReplace.parentNode?.insertBefore(replacer, toReplace)
+    if ('nodeType' in replacer) {
+      toReplace.parentNode?.insertBefore(replacer, toReplace)
+    } else {
+      const replacerNodes = Array.from(replacer)
+      replacerNodes.forEach(rpl => toReplace.parentNode?.insertBefore(rpl, toReplace))
+    }
     toReplace.parentNode?.removeChild(toReplace)
   })
   return inputElement

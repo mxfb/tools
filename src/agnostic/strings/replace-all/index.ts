f4d2f8e27ba60cc232197a891041cec74d5ddc84
Maxime Fabas <fabas@lemonde.fr>
2024-12-06 16:52:36 +0100

    ~/agnostic/strings/replace-all - rewrite/optimize

--- src/agnostic/strings/replace-all/index.ts
+++ src/agnostic/strings/replace-all/index.ts
@@ -1,14 +1,16 @@
 export function replaceAll (
   input: string,
   pattern: string | RegExp,
-  replacer: string | ((substring: string, ...args: any[]) => string),
-  maxOps: number = 1000) {
-  let output = input
-  let opsCnt = 0
-  while (output.match(pattern) && opsCnt < maxOps) {
-    opsCnt ++
-    if (typeof replacer === 'function') output = output.replace(pattern, replacer)
-    else output = output.replace(pattern, replacer)
-  }
-  return output
+  replacer: string | ((substring: string, ...args: any[]) => string)): string {
+  const found = Array.from(input.match(pattern) ?? [])
+  const foundReplaced = typeof replacer === 'string'
+    ? found.map(() => replacer)
+    : found.map(e => replacer(e))
+  const splitted = input.split(pattern)
+  const replaced = splitted.map((chunk, chunkPos) => {
+    const isLast = chunkPos === splitted.length - 1
+    if (isLast) return [chunk]
+    return [chunk, foundReplaced[chunkPos] ?? '']
+  }).flat().join('')
+  return replaced
 }

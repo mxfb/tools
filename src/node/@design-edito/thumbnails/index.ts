6de119cec7c2ffe6334ad4b08db2a1b3505ff420
itsleaht <leaztanda@gmail.com>
2025-05-14 11:42:24 +0200

    src/node/@design-edito/thumbnails: remove utils

--- src/node/@design-edito/thumbnails/index.ts
+++ src/node/@design-edito/thumbnails/index.ts
@@ -1,12 +1,3 @@
 // Exports individuels sans namespace 
-import * as Colors from './colors';
-import * as Easings from './easings';
-import * as Common from './common';
-import * as Layout from './layout';
-
 export {
-    Colors,
-    Easings,
-    Common,
-    Layout
 }
